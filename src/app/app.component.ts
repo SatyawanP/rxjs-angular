89cee136fb13019bc3182df308b749f0db15babe
satyawanphadtare <satyawan20phadtare@gmail.com>
2022-07-24 02:27:22 +0530

    added pipes, tap and map functions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, VERSION } from '@angular/core';
-import { from, of } from 'rxjs';
+import { from, map, of } from 'rxjs';
+import { tap } from 'rxjs/operators';
 
 @Component({
   selector: 'my-app',
@@ -15,10 +16,22 @@ export class AppComponent implements OnInit {
       next: (item) => console.log(item),
     });
 
-    from([5, 10, 15, 20]).subscribe({
-      next: (item) => console.log(`resulting item ${item}`),
-      error: (err) => console.log(`resulting item ${err}`),
-      complete: () => console.log('complete'),
-    });
+    from([5, 10, 15, 20, 4, 25])
+      .pipe(
+        tap((item) => console.log(`emitted item: ${item}`)),
+        map((item) => item * 2),
+        map((item) => item - 8),
+        map((item) => {
+          if (item == 0) {
+            throw new Error('zero detected');
+          }
+          return item;
+        })
+      )
+      .subscribe({
+        next: (item) => console.log(`resulting item ${item}`),
+        error: (err) => console.log(`resulting item ${err}`),
+        complete: () => console.log('complete'),
+      });
   }
 }
